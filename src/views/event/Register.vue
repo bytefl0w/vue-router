1c3e44aa994b476edbb5b78bbb6728636c39ab10
Erik Mortimer <erikmortimermail@gmail.com>
2021-02-06 12:36:12 -0500

    Added programmatic navigation with register button

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,11 +1,25 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 
 export default {
-  props: ["event"]
+  props: ["event"],
+  methods: {
+    // Call to API
+    // If registered then redirect to event details
+    register() {
+      this.$router.push({
+        name: 'EventDetails',
+        //params: { id: this.event.id }
+        // * Programmatic Navigation *
+        // NOTE: Both EventRegister and EventDetails have an :id param which will be used
+        // by default when navigating
+      })
+    }
+  }
 };
 </script>
 
